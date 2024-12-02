c3828c8075277a081c9742931b7b7d1ad9332033
Laura MONTAGNIER <laura.montagnier@ensta-paris.fr>
2024-12-02 10:16:40 +0100

    Méthodes question 3.1

--- src/main/java/com/ensta/myfilmlist/dao/FilmDAO.java
+++ src/main/java/com/ensta/myfilmlist/dao/FilmDAO.java
@@ -1,21 +1,9 @@
 package com.ensta.myfilmlist.dao;
 
-import com.ensta.myfilmlist.exception.ServiceException;
-import com.ensta.myfilmlist.model.Realisateur;
-import java.lang.Math;
-
 import java.util.List;
-import java.util.stream.Stream;
-import java.util.Arrays;
+import java.util.Optional;
 
-import com.ensta.myfilmlist.exception.ServiceException;
 import com.ensta.myfilmlist.model.Film;
-import com.ensta.myfilmlist.exception.ServiceException;
-import com.ensta.myfilmlist.model.Realisateur;
-import com.ensta.myfilmlist.persistence.ConnectionManager;
-import com.ensta.myfilmlist.service.MyFilmsService;
-
-import javax.sql.DataSource;
 
 
 
@@ -23,6 +11,11 @@ import javax.sql.DataSource;
 public interface FilmDAO {
 
     List<Film> findAll();
+    Optional<Film> findById(long id);
+    void delete(Film film);
+    List<Film> findByRealisateurId(long realisateurId);
+
+
 
 
 
