883508a0f4b7d9e6b9f23255989200c36ea8a55d
Laura MONTAGNIER <laura.montagnier@ensta-paris.fr>
2024-12-15 19:44:20 +0100

    Fin de la mise en place de Swagger (petit 4 partie 3)

--- src/main/java/com/ensta/myfilmlist/persistence/controller/FilmResource.java
+++ src/main/java/com/ensta/myfilmlist/persistence/controller/FilmResource.java
@@ -2,12 +2,32 @@ package com.ensta.myfilmlist.persistence.controller;
 
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import com.ensta.myfilmlist.dto.FilmDTO;
 import com.ensta.myfilmlist.exception.ControllerException;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+// L’API s’appelle « Film » et utilise le Tag « Film »
+// Le tag « Film » contient la description de l’API
+@Api(tags = "Film") 
+@Tag(name = "Film", description = "Opération sur les films")
+
+
 public interface FilmResource {
+
+    @ApiOperation(value = "Lister les films", notes = "Permet de renvoyer la liste de tous les films.", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+		@ApiResponse(code = 200, message = "La liste des films a été renvoyée correctement")
+    })
+
+
     public ResponseEntity<List<FilmDTO>> getAllFilms() throws ControllerException;
     public ResponseEntity<FilmDTO> getFilmById(long id) throws ControllerException;
 }
