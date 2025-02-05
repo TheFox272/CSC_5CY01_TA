68d90c4f421e5d0d9301475f0c4c714bcf36cbe9
blandinenani <blandinenani@gmail.com>
2025-02-05 16:21:53 +0100

    Header.js, App.js : dividing the components between different url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,24 @@ import Header from './Header';
 import FilmContainer from './components/FilmContainer';
 import Quizz from './components/Quizz';
 
+// pour le routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 //********************************************************************************************************************
 
+
+
 function App() {
-  return (
-    <>
-      <Header />
-      <FilmContainer />
-      <Quizz />
-    </>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<h1>Accueil</h1>} />
+                <Route path="/films" element={<FilmContainer />} />
+                <Route path="/quizz" element={<Quizz />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
