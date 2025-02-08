eb47dd9c073cb99adc7bd03173cc0fd49cd2223b
TheFox272 <fabien.girard27@gmail.com>
2025-02-09 00:17:11 +0100

    colonnes pour filmlist

--- frontend/src/components/FilmList.js
+++ frontend/src/components/FilmList.js
@@ -9,7 +9,22 @@ export default function FilmList(props) {
     // debug
     console.log(props.films);
 
-	return props.films.map(film => {
-    	return <FilmCard key={film.id} film={film} />
-    })
+    const half = Math.ceil(props.films.length / 2);
+    const firstHalf = props.films.slice(0, half);
+    const secondHalf = props.films.slice(half);
+
+    return (
+        <div style={{ display: 'flex' }}>
+            <div style={{ flex: 1 }}>
+                {firstHalf.map(film => (
+                    <FilmCard key={film.id} film={film} />
+                ))}
+            </div>
+            <div style={{ flex: 1 }}>
+                {secondHalf.map(film => (
+                    <FilmCard key={film.id} film={film} />
+                ))}
+            </div>
+        </div>
+    );
 }
