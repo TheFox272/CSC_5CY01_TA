d017b4fe4aa9662153b711d8cdf53ebdc6084458
Laura MONTAGNIER <laura.montagnier@ensta-paris.fr>
2025-02-09 20:41:01 +0100

    Petites améliorations dans les descriptions. (accents, etc...)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,23 +15,23 @@ INSERT INTO Realisateur(nom, prenom, date_naissance, celebre) VALUES('Spielberg'
 
 
 CREATE TABLE IF NOT EXISTS Film(id INT primary key auto_increment, titre VARCHAR(100), duree INT, realisateur_id INT, description VARCHAR(1000));
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('avatar', 162, 1,'Pocahontas mais dans l''espace');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('La communauté de l''anneau', 178, 2,'Sean Bean se fait encore fait fumer');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Les deux tours', 179, 2,'Un soleil rouge se lève, beaucoup de sang a du couler cette nui');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Le retour du roi', 201, 2,'Un mec trouve un anneau, son neveu le rapporte à l''usine');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Titanic', 201, 1,'Une vieille dame moribonde parle à ses enfants de son amour de jeunesse.');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Batman', 126, 3,'Un milliardaire fait du cosplay et tabasse des criminels');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('La Belle et la Bête', 147, 4,'Le syndrome de stockholm mais avec un animal');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('The social network', 120, 5,'Un film où tu as accès à la vie privée de Mark Zuckerberg au lieu de l''inverse');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Into the wild', 148, 6,'Il cuisine des champignons ca tourne mal');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Harry potter', 152, 7,'Un mec sans nez a une obsession malsaine pour un adolescent');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Matrix', 146, 8,'Un employé de bureau déprimé rejoint une secte et destabilise le gouvernement');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Star Wars prélogie', 146, 9,'Des prêtres achetent un enfant pour le former au culte et il se marie avec une politicienne de deux fois son age');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Narnia', 143, 10,'Les enfants sortent du placard');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('The Human centipede', 92, 11,'Un médecin d''une petite ville décide de rapprocher les gens');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Taken', 93, 12,'Liam Neeson doit aller chercher sa fille');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('E.T', 120, 13,'Un immigré clandestin est poursuivi par les forces de l''ordre');
-INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Charlie et la chocolaterie', 115, 3,'Un chocolatier lunatique tue lentement des enfants devant leur parents');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Avatar', 162, 1,'Pocahontas mais dans l''espace avec des grands Schtroumpfs.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('La communauté de l''anneau', 178, 2,'Sean Bean se fait encore fait fumer.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Les deux tours', 179, 2,'Un soleil rouge se lève, beaucoup de sang a dû couler cette nuit.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Le retour du roi', 201, 2,'Un mec trouve un bijou, son neveu le rapporte à l''usine'.);
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Titanic', 201, 1,'Plouf !');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Batman', 126, 3,'Un milliardaire se déguise en animal et tabasse un clown.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('La Belle et la Bête', 147, 4,'Le syndrome de Stockholm mais avec un animal.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('The social network', 120, 5,'Un film où tu as accès à la vie privée de Mark Zuckerberg au lieu de l''inverse.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Into the wild', 148, 6,'Il cuisine des champignons, ça tourne mal.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Harry potter', 152, 7,'Un mec sans nez a une obsession malsaine pour un adolescent.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Matrix', 146, 8,'Un employé de bureau déprimé rejoint une secte et déstabilise le gouvernement.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Star Wars prélogie', 146, 9,'Des prêtres achètent un enfant pour le former au culte et il se marie avec une politicienne de deux fois son âge.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Narnia', 143, 10,'Les enfants sortent du placard.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Fight Club', 92, 5,'Un mec se met des gifles tout seul.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Taken', 93, 12,'Liam Neeson doit aller chercher sa fille.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('E.T.', 120, 13,'Un immigré clandestin est poursuivi par les forces de l''ordre.');
+INSERT INTO Film(titre, duree, realisateur_id,description) VALUES('Charlie et la chocolaterie', 115, 3,'Un chocolatier lunatique tue lentement des enfants devant leur parents.');
 
 
 
